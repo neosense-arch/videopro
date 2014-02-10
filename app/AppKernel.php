7aea77f7088c58825e97b793089e762d252558ff
Yury Stmol <dev@stmol.me>
2014-02-10 12:26:37 +0400

    Initial commit

---
+++ app/AppKernel.php
@@ -0,0 +1,19 @@
+<?php
+
+use NS\CoreBundle\CoreKernel;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * App Kernel
+ *
+ */
+class AppKernel extends CoreKernel
+{
+	/**
+	 * @return Bundle[]
+	 */
+	protected function getUserBundles()
+	{
+		return array();
+	}
+}
